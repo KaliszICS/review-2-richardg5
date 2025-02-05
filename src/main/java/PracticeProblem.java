3a570a7905b9aaf14aebdb17ba8b00ac340af2d4
richardg5 <richard.gao5@student.tdsb.on.ca>
2025-02-05 18:24:48 +0000

    Add code

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -10,33 +12,59 @@ public class PracticeProblem {
 	}
 
 	public static void q1() {
-		//Write question 1 code here
-		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("In: ");
+		String in = scanner.nextLine();
+		System.out.println(in);
+		scanner.close();
 	}
 
 	public static void q2() {
-		//Write question 2 code here
-		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("In: ");
+		int a = scanner.nextInt();
+		scanner.nextLine();
+		System.out.print("In: ");
+		int b = scanner.nextInt();
+		scanner.nextLine();
+		System.out.println(a / b);
+		System.out.println(a % b);
+		scanner.close();
 	}
 
 	public static void q3() {
-		//Write question 3 code here
-		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("In: ");
+		String in = scanner.nextLine();
+		System.out.println(in + " was the text you wrote");
+		scanner.close();
 	}
 
 	public static void q4() {
-		//Write question 4 code here
-		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("In: ");
+		int n = scanner.nextInt();
+		scanner.nextLine();
+		System.out.println(n * 5);
+		scanner.close();
 	}
 
 	public static void q5() {
-		//Write question 5 code here
-		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("In: ");
+		boolean b = scanner.nextBoolean();
+		scanner.nextLine();
+		System.out.println(b + " is a boolean");
+		scanner.close();
 	}
 
 	public static void q6() {
-		//Write question 6 code here
-		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("In: ");
+		double d = scanner.nextDouble();
+		scanner.nextLine();
+		System.out.println(d - 3.2);
+		scanner.close();
 	}
 
 }
